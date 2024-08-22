176b56e3419b67dc33f4ab9e1fb40c98c4003b74
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-08-22 23:07:10 +0530

    leetcode138, CopyListWithRandomPointer138

--- Leetcode Company Wise/Microsoft/CopyListWithRandomPointer138.cpp
+++ Leetcode Company Wise/Microsoft/CopyListWithRandomPointer138.cpp
@@ -14,6 +14,14 @@ public:
 };
 */
 
+// Intuiton and Approach - 
+
+// first i created a map that will map the current values to the nodes containing same values. 
+
+// now we can simply travel through and mark the next and random pointer for all the nodes to the , actual nodes with next and random pointers. 
+
+// Like mp[curr]->next - it implies the duplicate node we created with same value and change its next to mp[curr->next] means - its next should be equal the next of curr but we are pointing to duplicate value. 
+
 
 
 class Solution {
