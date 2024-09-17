f8f0c2eb204df0832f4b4026d864b50a47fb7afd
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-09-18 01:37:25 +0530

    CoinPiles cses

---
+++ CSES/Introduction/CoinPiles.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int t; cin >> t;
+  while(t--){
+    int a,b; cin>>a>>b;
+
+    if(a > 2*b || b > 2*a ) cout << "NO" << endl;
+    else{
+      if((a+b)%3 == 0) cout << "YES"<<endl;
+      else cout << "NO" << endl;
+    }
+    
+  }
+}
